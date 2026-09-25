1fdbca5449f8082ed2ecfd39de02310ef680eb0b
Sefa Sahin <yazilimyazilim81@gmail.com>
2026-09-25 10:40:54 +0300

    Created sell method in product class.

--- Product.cs
+++ Product.cs
@@ -1,6 +1,24 @@
 public class Product{
 	public String Name{get; set;}
 	public decimal Price {get; set;}
-	public int Stock {get; set;}
-	public bool isAvailable {get; set;}
+	public int Stock {get; private set;}
+	public bool IsAvailable => Stock > 0;
+	
+	public Product(string name, decimal price, int stock){
+		Name = name;
+		Price = price;
+		if(stock < 0){
+			throw new ArgumentException("Stock cannot be negative");
+		}
+		Stock = stock;
+	}
+	
+	public void Sell(int quantity){
+		if(quantity > Stock || quantity <= 0){
+			throw new ArgumentException("Quantity cannot be higher than Stock");
+		}
+		if(Stock >= quantity){
+			Stock -=  quantity;
+		}
+	}
 }
--- Program.cs
+++ Program.cs
@@ -1,59 +1,14 @@
-int id = 1;
-String name = "MacBook Pro M1Pro";
-decimal stockQuantity = 3.0m;
-bool isAvailable = true;
-Console.WriteLine($"Product id:{id}");
-Console.WriteLine($"Product name: {name}");
-int secondId = 2;
-String secondName = "iPhone 17";
-decimal secondStock = 2.0m;
-bool secondIsAvailable = false;
-Console.WriteLine($"Second product name: {secondName}");
-Console.WriteLine($"Second product stock: {secondStock}");
-
-
-Product product1 = new Product();
-product1.Name = "Macbook Pro";
-product1.Price = 1350.0m;
-product1.Stock = 23;
-product1.isAvailable = true;
-Console.WriteLine($"Product name : {product1.Name}");
-Console.WriteLine($"Product price : {product1.Price}");
-Console.WriteLine($"Product stock : {product1.Stock}");
-Console.WriteLine($"Available : {product1.isAvailable}"); 
-
-Product product2 = new Product();
-product2.Name = "iPhone 17";
-product2.Price = 1400.0m;
-product2.Stock = 55;
-product2.isAvailable = true;
-Console.WriteLine($"Product name : {product2.Name}");
-Console.WriteLine($"Product price: {product2.Price}");
-Console.WriteLine($"Product stock : {product2.Stock}");
-Console.WriteLine($"Available : {product2.isAvailable}");
-List<Product> productList = new List<Product>();
-productList.Add(product1);
-productList.Add(product2);
-
-foreach(Product product in productList){
-	Console.WriteLine($"Products name : {product.Name}");
-	Console.WriteLine($"Products price : {product.Price}");
+Product product1 = new Product("iPhone 17", 1400m, 15);
+Product product2 = new Product("MacBook Air", 1000m, 0);
+Product product3 = new Product("iPad Air", 1250m, 25);
+List<Product> products = new List<Product>();
+products.Add(product1);
+products.Add(product2);
+products.Add(product3);
+foreach(Product product in products){
+	Console.WriteLine($"Product description: {product.Name} -- {product.Price} -- {product.Stock} \n");
 }
 
-var filteredProducts = productList.Where(product => product.Stock >= 30);
-foreach(Product product in filteredProducts){
-	Console.WriteLine($"Product name: {product.Name}");
+foreach(Product product in products){
+        Console.WriteLine($"Product description: {product.Name} -- {product.Price} -- {product.Stock} \n");
 }
-
-var expensiveProducts = productList.Where(product => product.Price > 1350);
-foreach(Product product in expensiveProducts){
-	Console.WriteLine($"Product name and price : {product.Name}, {product.Price}");
-}
-
-var orderedProducts = productList.OrderBy(product => product.Price);
-foreach(Product product in orderedProducts){
-	Console.WriteLine($"Listed {product.Price}");
-}
-
-Product? foundProducts = productList.FirstOrDefault(product => product.Name == "iPhone 17");
-Console.WriteLine($"Stock count : {foundProducts.Stock}");
